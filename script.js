e87765ac228dc19811a3fab18f5c1fbf5ebe0e3a
choubhareyash3112 <91589038+choubhareyash3112@users.noreply.github.com>
2025-11-05 21:08:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //your JS code here. If required.
-let ptag=document.createElement("p");
+let ptag=document.getElementById("timer");
 let now = new Date();
 
  ptag.textContent=now.toLocaleString();
-document.body.appendChild(ptag);
